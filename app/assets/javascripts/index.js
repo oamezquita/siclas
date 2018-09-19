f9f16fbc3bb76ec548af1b5c537b868b48561510
oscar <oamezquita@hotmail.es>
2018-09-19 12:49:50 -0500

    se agregan js

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,23 @@
+$('.carousel').carousel({
+    interval: 4000
+    });
+
+    jQuery(document).ready(function() {
+    jQuery(".itemJQuery").bind({
+        mouseenter: function() {
+            var self = jQuery(this), billboard = self.data("billboardElement");
+            if(!billboard) {
+                billboard = jQuery(jQuery(".item-billboard", this)[0]);
+                self.data("billboardElement", billboard);
+            }
+            jQuery(billboard).stop().animate({
+                "margin-top": "-240px"
+            });
+        },
+        mouseleave: function() {
+            jQuery(this).data("billboardElement").stop().animate({
+                "margin-top": 0
+            });
+        }
+    });
+});	
